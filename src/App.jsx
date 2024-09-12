e2a22dab03e440a9483f37701ec75125f8d6560f
Ekarahma148 <ekaarahma010@gmail.com>
2024-09-12 14:07:11 +0700

    kuis 3

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Profil from "./Profil"
+
+function App(){
+  return(
+<>
+<div className="container">
+  <h2>Profil</h2>
+<Profil/>
+<p>Eka Rahma</p>
+<p>14 Agustus 2004</p>
+<p>Banjar, Jawa Barat</p>
+</div>
+</>
+  );
+}
+export default App;
---
+++ src/Profil.jsx
@@ -0,0 +1,5 @@
+function Profil(){
+    <img src="https://i.imgur.com/MK3eW3As.jpg" alt="kosong"></img>
+
+}
+export default Profil;
